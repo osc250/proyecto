f1dfd72d0143671c0fa285bbbf5594df7dee6ad3
osc250 <81557180+osc250@users.noreply.github.com>
2021-04-07 19:16:08 -0500

    Add files via upload

---
+++ script/contacto.js
@@ -0,0 +1,6 @@
+(function () {
+    
+    // Get the registration <form> element from the DOM.
+    var form = document.getElementById("registration-form");
+    var submitButton = form.querySelector("button");
+})
